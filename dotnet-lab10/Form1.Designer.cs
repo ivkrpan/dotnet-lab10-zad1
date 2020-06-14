75777dd362145840bf68139a076ad55c7667f05e
ivkrpan <ivkrpan@vsite.hr>
2020-06-14 17:17:22 +0200

    lab 10 zad1

--- dotnet-lab10/Form1.Designer.cs
+++ dotnet-lab10/Form1.Designer.cs
@@ -28,12 +28,72 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnObradi = new System.Windows.Forms.Button();
+            this.btnZaustavi = new System.Windows.Forms.Button();
+            this.pbStatus = new System.Windows.Forms.ProgressBar();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnObradi
+            // 
+            this.btnObradi.Location = new System.Drawing.Point(62, 46);
+            this.btnObradi.Name = "btnObradi";
+            this.btnObradi.Size = new System.Drawing.Size(75, 23);
+            this.btnObradi.TabIndex = 0;
+            this.btnObradi.Text = "Obrađuj";
+            this.btnObradi.UseVisualStyleBackColor = true;
+            this.btnObradi.Click += new System.EventHandler(this.btnObradi_Click);
+            // 
+            // btnZaustavi
+            // 
+            this.btnZaustavi.Location = new System.Drawing.Point(152, 46);
+            this.btnZaustavi.Name = "btnZaustavi";
+            this.btnZaustavi.Size = new System.Drawing.Size(75, 23);
+            this.btnZaustavi.TabIndex = 1;
+            this.btnZaustavi.Text = "Prekini";
+            this.btnZaustavi.UseVisualStyleBackColor = true;
+            this.btnZaustavi.Click += new System.EventHandler(this.btnZaustavi_Click);
+            // 
+            // pbStatus
+            // 
+            this.pbStatus.Location = new System.Drawing.Point(62, 102);
+            this.pbStatus.Name = "pbStatus";
+            this.pbStatus.Size = new System.Drawing.Size(384, 23);
+            this.pbStatus.TabIndex = 2;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(59, 86);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(0, 13);
+            this.lblStatus.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(509, 176);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.pbStatus);
+            this.Controls.Add(this.btnZaustavi);
+            this.Controls.Add(this.btnObradi);
+            this.Name = "Form1";
+            this.Text = "Pozadinska obrada";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnObradi;
+        private System.Windows.Forms.Button btnZaustavi;
+        private System.Windows.Forms.ProgressBar pbStatus;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label lblStatus;
     }
 }
 
--- dotnet-lab10/Form1.cs
+++ dotnet-lab10/Form1.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -15,6 +16,70 @@ namespace dotnet_lab10
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            backgroundWorker1.DoWork += (o, a) =>
+            {
+
+                for (int i = 1; i <= 100; i++)
+                {
+
+                    if ((backgroundWorker1.CancellationPending == true))
+                    {
+                        a.Cancel = true;
+                        return;
+                    }
+                    else
+                    {
+                        Thread.Sleep(500);
+                        backgroundWorker1.ReportProgress(i);
+                    }
+
+                }
+                a.Result = "Gotovo!";
+            };
+
+            backgroundWorker1.RunWorkerCompleted += (o, a) =>
+            {
+                if (a.Cancelled)
+                {
+                    lblStatus.Text = "Prekinuto";
+                    pbStatus.Value = 0;
+                }
+                else
+                    lblStatus.Text = a.Result.ToString();
+            };
+
+            backgroundWorker1.ProgressChanged += (o, a) =>
+            {
+                lblStatus.Text = a.ProgressPercentage + "%";
+                pbStatus.Value = a.ProgressPercentage;
+            };
+
+            backgroundWorker1.WorkerReportsProgress = true;
+            backgroundWorker1.WorkerSupportsCancellation = true;
+        }
+
+        private void btnObradi_Click(object sender, EventArgs e)
+        {
+            if (backgroundWorker1.IsBusy == false)
+            {
+                pbStatus.Value = 0;
+                lblStatus.Text = "0%";
+                backgroundWorker1.RunWorkerAsync();
+                
+            }
+               
+        }
+
+        private void btnZaustavi_Click(object sender, EventArgs e)
+        {
+
+            if (backgroundWorker1.IsBusy)
+                backgroundWorker1.CancelAsync();
         }
     }
 }
